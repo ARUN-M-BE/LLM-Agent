c6ace85f6048514c43f33e09f6545e852fc7bf3f
lovelyarun1023 <lovelyarun1023@gmail.com>
2025-12-04 06:55:43 +0000

    creation

---
+++ server/server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/llm-agent', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
