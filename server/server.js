3d8e6d1dc2728d2249b15467550a73669668a6b0
lovelyarun1023 <lovelyarun1023@gmail.com>
2025-12-04 07:14:51 +0000

    Changes before Firebase Studio auto-run

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
